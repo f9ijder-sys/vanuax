faf4b3a00cf83554fb2a317a654fffd9ba19fa99
f9ijder-sys <f9ijder@gmail.com>
2025-09-01 02:49:28 +0100

    Update and rename src/index.jsx to main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import VanyxasShop from './VanyxasShop'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VanyxasShop />
+  </React.StrictMode>,
+)
